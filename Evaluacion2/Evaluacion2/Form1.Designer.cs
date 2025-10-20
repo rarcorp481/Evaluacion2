5352f7cafb93473e106df9c625a472a6e98da6d9
Dubua <rramirezc@uamv.edu.ni>
2025-10-20 07:55:59 -0600

    add frontend

---
+++ Evaluacion2/Evaluacion2/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Evaluacion2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Datos = new System.Windows.Forms.TextBox();
+            this.lblDatos = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.Contenido = new System.Windows.Forms.ListBox();
+            this.lblAdvertencia = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Datos
+            // 
+            this.Datos.Location = new System.Drawing.Point(118, 74);
+            this.Datos.Name = "Datos";
+            this.Datos.Size = new System.Drawing.Size(245, 26);
+            this.Datos.TabIndex = 0;
+            // 
+            // lblDatos
+            // 
+            this.lblDatos.AutoSize = true;
+            this.lblDatos.Location = new System.Drawing.Point(52, 77);
+            this.lblDatos.Name = "lblDatos";
+            this.lblDatos.Size = new System.Drawing.Size(60, 20);
+            this.lblDatos.TabIndex = 1;
+            this.lblDatos.Text = "Datos: ";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(415, 53);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(136, 68);
+            this.btnAgregar.TabIndex = 2;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(577, 53);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(143, 68);
+            this.btnEliminar.TabIndex = 3;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Contenido
+            // 
+            this.Contenido.FormattingEnabled = true;
+            this.Contenido.ItemHeight = 20;
+            this.Contenido.Location = new System.Drawing.Point(56, 174);
+            this.Contenido.Name = "Contenido";
+            this.Contenido.Size = new System.Drawing.Size(664, 204);
+            this.Contenido.TabIndex = 4;
+            // 
+            // lblAdvertencia
+            // 
+            this.lblAdvertencia.AutoSize = true;
+            this.lblAdvertencia.Location = new System.Drawing.Point(56, 124);
+            this.lblAdvertencia.Name = "lblAdvertencia";
+            this.lblAdvertencia.Size = new System.Drawing.Size(97, 20);
+            this.lblAdvertencia.TabIndex = 5;
+            this.lblAdvertencia.Text = "Advertencia:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblAdvertencia);
+            this.Controls.Add(this.Contenido);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.lblDatos);
+            this.Controls.Add(this.Datos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Datos;
+        private System.Windows.Forms.Label lblDatos;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.ListBox Contenido;
+        private System.Windows.Forms.Label lblAdvertencia;
+    }
+}
+
